a1f68159073db4e1f3113890d3ab03f196282847
Yukta Jotiba  Patil <yuktapatil1820@gmail.com>
2023-10-21 23:02:19 +0530

    Task 7

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
-//const http1 = require("./http.js"); 
 const http1 = require("http");
 
-// function rqListener(req, res){
 
-// }
-// http1.createServer(rqListener);  
 
-const server  = http1.createServer((req,res)=>{
 
-    console.log("Yukta Patil");
+const server = http1.createServer((req, res) => {
+
+    const url = req.url;
+
+    if (url === '/') {
+
+        res.write("<html>");
+
+        res.write('<head><title>Enter Message</title></head>');
+
+        res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form></body>');
+
+        res.write('</html>');
+
+        return res.end();
+
+    }
+
+    if (url === "/home") {
+
+        res.write("<html>");
+
+        res.write('<head><title>My first Page</title></head>');
+
+        res.write('<body><h1>Welcome home Yukta!!</h1></body>');
+
+        res.write('</html>');
+
+        return res.end();
+
+    }
+
+    if (url === "/about") {
+
+        res.write("<html>");
+
+        res.write('<head><title>My About page</title></head>');
+
+        res.write('<body><h1>Welcome To about us Page</h1></body>');
+
+        res.write('</html>');
+
+        return res.end();
+
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write("<html>");
+
+    res.write('<head><title>My first Page</title></head>');
+
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+
+    res.write('</html>');
+
+    res.end();
+
+
+
+
+    //process.exit();
+
 })
-server.listen(4000);
+
+server.listen(3000);
